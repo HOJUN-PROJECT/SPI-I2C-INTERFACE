eed9f3466704fcc389af93bb7cad915fede9ff40
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-17 20:16:33 +0900

    Update Slave.sv

--- I2C/Slave.sv
+++ I2C/Slave.sv
@@ -1,180 +1,227 @@
 `timescale 1ns / 1ps
 
-module I2C_Slave (
-    input        clk,
-    input        SRESET,
-    input        SCL,
-    inout        SDA,
-    output [7:0] LED
+module I2C_SLAVE (
+    // global signals
+    input  logic       clk,
+    input  logic       reset,
+    // Internal signals
+    input  logic [7:0] tx_data,
+    output logic       tx_done,
+    output logic       tx_ready,
+    output logic [7:0] rx_data,
+    output logic       rx_done,
+    // External signals
+    input  logic       scl,
+    inout  logic       sda
 );
 
-    localparam Slave_ADDR = 7'b110_0000;
+    logic sda_en;
+    logic o_sda;
+    logic [7:0] addr_reg, addr_next;
+    logic [7:0] rx_data_reg, rx_data_next;
+    logic [7:0] tx_data_reg, tx_data_next;
+    logic burst_read_reg, burst_read_next;
+    
+    ///////////////////////synchronizer && edge detector////////////////////////////////
+    logic sda_falling, sda_rising;
+    logic scl_falling, scl_rising;
+    logic sda_sync0, sda_sync1;
+    logic scl_sync0, scl_sync1;
 
-    typedef enum logic [3:0] {
-        START,
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            sda_sync0 <= 0;
+            sda_sync1 <= 0;
+            scl_sync0 <= 0;
+            scl_sync1 <= 0;
+        end else begin
+            sda_sync0 <= sda;
+            sda_sync1 <= sda_sync0;
+            scl_sync0 <= scl;
+            scl_sync1 <= scl_sync0;
+        end
+    end
+
+    assign sda_rising  = sda_sync0 && (~sda_sync1);
+    assign sda_falling = (~sda_sync0) && sda_sync1;
+    assign scl_rising  = scl_sync0 && (~scl_sync1);
+    assign scl_falling = (~scl_sync0) && scl_sync1;
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    assign sda         = (sda_en) ? o_sda : 1'bz;
+
+    typedef enum {
         IDLE,
         ADDR,
-        DATA,
-        WRITE,
-        STOP,
-        HOLD,
+        SEND_ACK,
         WAIT,
-        STORE,
-        SS
-    } state_t;
+        SEND_DATA,
+        RCV_DATA,
+        RCV_ACK,
+        RCV_ACK_HOLD,
+        WAIT_2,
+        SEND_ACK_2,
+        STOP
+    } state_e;
 
-    state_t state, state_next;
+    state_e state, state_next;
 
-    logic sda_next, sda_reg, sclk_sync0, sclk_sync1, SDA_en, SDA_en_next;
-    logic [7:0] addr_next, addr_reg;
-    logic [7:0] led_next, led_reg;
-    logic [3:0] bit_counter_next, bit_counter_reg;
-    logic [11:0] clk_counter_next, clk_counter_reg;
-    logic sclk_rising, sclk_falling;
+    logic [2:0] bit_cnt_reg, bit_cnt_next;
+    logic rx_done_reg, rx_done_next;
+    wire [6:0] slave_addr = 7'b1111110;
 
-    assign SDA = (SDA_en) ? sda_reg : 1'bz;
-    assign LED = led_reg;
-
-    always @(posedge clk, posedge SRESET) begin
-        if (SRESET) begin
-            sclk_sync0 <= 0;
-            sclk_sync1 <= 0;
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            state          <= IDLE;
+            bit_cnt_reg    <= 0;
+            addr_reg       <= 0;
+            rx_data_reg    <= 0;
+            rx_done_reg    <= 0;
+            tx_data_reg    <= 0;
+            burst_read_reg <= 0;
         end else begin
-            sclk_sync0 <= SCL;
-            sclk_sync1 <= sclk_sync0;
+            state          <= state_next;
+            bit_cnt_reg    <= bit_cnt_next;
+            addr_reg       <= addr_next;
+            rx_data_reg    <= rx_data_next;
+            rx_done_reg    <= rx_done_next;
+            tx_data_reg    <= tx_data_next;
+            burst_read_reg <= burst_read_next;
+
         end
     end
 
-    assign sclk_rising  = (sclk_sync0 && ~sclk_sync1);
-    assign sclk_falling = (~sclk_sync0 && sclk_sync1);
+    always_comb begin
+        state_next      = state;
+        bit_cnt_next    = bit_cnt_reg;
+        addr_next       = addr_reg;
+        rx_data_next    = rx_data_reg;
+        rx_done_next    = 0;
+        o_sda           = 1'b1;  // 기본 값 1??
+        tx_data_next    = tx_data_reg;
+        burst_read_next = burst_read_reg;
+        tx_done         = 0;
 
-    always @(posedge clk, posedge SRESET) begin
-        if (SRESET) begin
-            state           <= IDLE;
-            bit_counter_reg <= 0;
-            led_reg         <= 0;
-            sda_reg         <= 0;
-            addr_reg        <= 0;
-            clk_counter_reg <= 0;
-            SDA_en          <= 0;
-        end else begin
-            state           <= state_next;
-            bit_counter_reg <= bit_counter_next;
-            led_reg         <= led_next;
-            sda_reg         <= sda_next;
-            addr_reg        <= addr_next;
-            clk_counter_reg <= clk_counter_next;
-            SDA_en          <= SDA_en_next;
+        if (state != IDLE) begin
+            if (scl_sync1 && sda_rising) begin
+                state_next = STOP;
+            end else if (scl_sync1 && sda_falling) begin
+                state_next   = ADDR;
+                bit_cnt_next = 0;
+            end
         end
-    end
-
-    always @(*) begin
-        state_next       = state;
-        bit_counter_next = bit_counter_reg;
-        led_next         = led_reg;
-        sda_next         = sda_reg;
-        SDA_en_next      = SDA_en;
-        addr_next        = addr_reg;
-        clk_counter_next = clk_counter_reg;
         case (state)
             IDLE: begin
-                bit_counter_next = 0;
-                SDA_en_next      = 0;
-                if (SCL && !SDA) begin
-                    state_next = HOLD;
-                end
-            end
-            HOLD: begin
-                if (clk_counter_reg == 499) begin
-                    clk_counter_next = 0;
-                    state_next       = START;
-                end else begin
-                    clk_counter_next = clk_counter_reg + 1;
-                end
-            end
-            START: begin
-                SDA_en_next = 0;
-                if (sclk_falling) begin
-                    addr_next = {addr_reg[6:0], SDA};
-                    if (bit_counter_reg == 7) begin
-                        bit_counter_next = 0;
-                        state_next = ADDR;
-                    end else begin
-                        bit_counter_next = bit_counter_reg + 1;
-                    end
+                rx_data_next = 0;
+                if (scl && sda_falling) begin
+                    bit_cnt_next = 0;
+                    state_next   = ADDR;
                 end
             end
             ADDR: begin
-                SDA_en_next = 0;
-                if (clk_counter_reg == 249) begin
-                    $display("[%t] Slave saw ADDR = %b", $time, addr_reg);
-                    if (addr_reg[6:0] == Slave_ADDR) begin
-                        state_next = WRITE;
+                if (scl_rising) begin
+                    addr_next = {addr_reg[6:0], sda};
+                    if (bit_cnt_reg == 7) begin
+                        state_next   = WAIT;
+                        rx_done_next = 1;
+                        bit_cnt_next = 0;
                     end else begin
-                        state_next = STOP;
-                    end
-                    clk_counter_next = 0;
-                end else begin
-                    clk_counter_next = clk_counter_reg + 1;
-                end
-            end
-            WRITE: begin
-                SDA_en_next = 1;
-                sda_next    = 1'b0;
-                if (clk_counter_reg == 1245) begin
-                    clk_counter_next = 0;
-                    state_next       = WAIT;
-                end else begin
-                    clk_counter_next = clk_counter_reg + 1;
-                end
-            end
-            WAIT: begin
-                SDA_en_next = 0;
-                if (clk_counter_reg == 1001) begin
-                    if (SCL == 1) begin
-                        state_next = STOP;
-                    end else begin
-                        state_next = DATA;
-                    end
-                    clk_counter_next = 0;
-                end else begin
-                    clk_counter_next = clk_counter_reg + 1;
-                end
-            end
-            DATA: begin
-                SDA_en_next = 0;
-                if (sclk_falling) begin
-                    led_next = {SDA, led_reg[7:1]};
-                    if (bit_counter_reg == 7) begin
-                        bit_counter_next = 0;
-                        state_next = STORE;
-                    end else begin
-                        bit_counter_next = bit_counter_reg + 1;
+                        bit_cnt_next = bit_cnt_reg + 1;
                     end
                 end
             end
-            STORE: begin
-                SDA_en_next = 0;
-                if (clk_counter_reg == 249) begin
-                    clk_counter_next = 0;
-                    state_next = SS;
-                end else begin
-                    clk_counter_next = clk_counter_reg + 1;
+            WAIT: begin  // rising_edge에서 바로 SEDN_ACK로 가버리면 충돌남
+                if (scl_falling) begin
+                    state_next = SEND_ACK;
                 end
             end
-            SS: begin
-                SDA_en_next = 1;
-                sda_next    = 1'b0;
-                if (clk_counter_reg == 996) begin
-                    clk_counter_next = 0;
-                    state_next       = WAIT;
-                end else begin
-                    clk_counter_next = clk_counter_reg + 1;
+            SEND_ACK: begin
+                if (addr_reg[7:1] == slave_addr) begin
+                    o_sda = 0;
+                end
+                if (scl_falling) begin
+                    if (addr_reg[7:1] == slave_addr) begin
+                        addr_next = 0;
+                        if (addr_reg[0]) begin
+                            state_next   = SEND_DATA;  // READ 동작
+                            tx_data_next = tx_data;
+                        end else begin
+                            state_next = RCV_DATA;  // Write 동작
+                        end
+                    end else begin
+                        state_next = STOP;  // 주소 불일치
+                    end
+                end
+            end
+            RCV_DATA: begin
+                if (scl_rising) begin
+                    rx_data_next = {rx_data_reg[6:0], sda};
+                    if (bit_cnt_reg == 7) begin
+                        state_next   = WAIT_2;
+                        bit_cnt_next = 0;
+                    end else begin
+                        bit_cnt_next = bit_cnt_reg + 1;
+                    end
+                end
+            end
+            WAIT_2: begin  // rising_edge에서 바로 SEDN_ACK로 가버리면 충돌남
+                if (scl_falling) begin
+                    state_next = SEND_ACK_2;
+                end
+            end
+            SEND_ACK_2: begin
+                o_sda = 0;
+                if (scl_falling) begin
+                    rx_done_next = 1'b1;  // rx_done 1 tick
+                    rx_data_next = 0;
+                    state_next   = RCV_DATA;
+                end
+            end
+            SEND_DATA: begin
+                o_sda = tx_data_reg[7];
+                if (scl_falling) begin
+                    if (bit_cnt_reg == 7) begin
+                        state_next = RCV_ACK;
+                        tx_done    = 1;
+                    end else begin
+                        tx_data_next = {tx_data_reg[6:0], 1'b0};
+                        bit_cnt_next = bit_cnt_reg + 1;
+                    end
+                end
+            end
+            RCV_ACK: begin
+                if (scl_rising) begin
+                    state_next = RCV_ACK_HOLD;
+                    if (!sda_sync1) begin
+                        burst_read_next = 1;
+                        bit_cnt_next = 0;
+                    end else begin
+                        burst_read_next = 0;
+                        bit_cnt_next = 0;
+                    end
+                end
+            end
+            RCV_ACK_HOLD: begin
+                if (scl_falling) begin
+                    if (burst_read_reg) begin
+                        state_next   = SEND_DATA;
+                        tx_data_next = tx_data;
+                    end 
+                    // else begin
+                    //     state_next = STOP;
+                    // end
                 end
             end
             STOP: begin
-                state_next = IDLE;
+                rx_data_next = 0;
+                state_next   = IDLE;
             end
         endcase
     end
+
+    assign sda_en  = (state == SEND_ACK) || (state == SEND_ACK_2) || (state == SEND_DATA);
+    assign rx_done = rx_done_reg;
+    assign rx_data = rx_data_reg;
+
+
 endmodule
